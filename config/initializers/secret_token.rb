131381a6d3df6b03fb63716aa716dbdee8d824da
samayaaan <samayaaan@gmail.com>
2013-07-08 01:20:42 +0900

    first commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TimeyHatebu::Application.config.secret_token = '7acaf63b9c8665e4748e86072db593a464aa540f147a8dac59b37b4cc43f7a67b284c19d3bd5c24fde23525239118626d086f97e7ce4ab5b2773194daf88d9b3'

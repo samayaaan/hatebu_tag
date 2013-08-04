987ab1b198d45c2337492d780d99fa0a76c3efa3
samayaaan <samayaaan@gmail.com>
2013-08-04 22:52:48 +0900

    herokuでのdb:migrate時エラー回避

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,9 @@ TimeyHatebu::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # heroku db:migrate時エラー対策
+  # http://help.papertrailapp.com/discussions/questions/116-logging-from-heroku-apps-using-unicorn
+  config.logger = Logger.new(STDOUT)
+  config.logger.level = Logger.const_get(ENV['LOG_LEVEL'] ? ENV['LOG_LEVEL'].upcase : 'INFO')
 end
